0de539a523160191ddf53d1273e0c122a8ab5d46
Adrien Grsmto <grsmto@gmail.com>
2017-09-26 18:18:41 +0100

    update symlinks for ST3

--- install/symlinks.sh
+++ install/symlinks.sh
@@ -2,5 +2,4 @@ ln -sfv "$DOTFILES_DIR/dots/.exports" ~
 ln -sfv "$DOTFILES_DIR/dots/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/dots/.gitignore_global" ~
 ln -sfv "$DOTFILES_DIR/dots/.bash_profile" ~
-ln -sfv "$DOTFILES_DIR/editors/Default (OSX).sublime-keymap" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Default (OSX).sublime-keymap"
-ln -sfv "$DOTFILES_DIR/editors/Preferences.sublime-settings" "$HOME/Library/Application Support/Sublime Text 3/Packages/User/Preferences.sublime-settings"
+ln -sfv "$DOTFILES_DIR/editors/Sublime Text 3/" "$HOME/Library/Application Support/Sublime Text 3/Packages/"

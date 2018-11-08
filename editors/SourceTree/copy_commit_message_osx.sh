dceb63908afbc924a3f2251193688deb4f6904db
Adrien Denat <oi@adriendenat.com>
2018-11-08 01:15:09 +0100

    update apps installed

--- editors/SourceTree/copy_commit_message_osx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# SourceTree Custom Action - Copy Commit Message to Clipboard
-# Copies the commit message of the selected commit to the clipboard
-#
-# SourceTree Parameters: $SHA
-
-git log $1 -1 --format=%B|pbcopy
--- install/brew.sh
+++ install/brew.sh
@@ -35,17 +35,18 @@ brew tap caskroom/versions
 # Install cask packages
 
 brew cask install --appdir="/Applications" google-chrome
+brew cask install --appdir="/Applications" firefox
 brew cask install --appdir="/Applications" slack
 brew cask install --appdir="/Applications" spotify
-brew cask install --appdir="/Applications" sublime-text
+brew cask install --appdir="/Applications" visual-studio-code
 brew cask install --appdir="/Applications" postman
 brew cask install --appdir="/Applications" spectacle
 brew cask install --appdir="/Applications" appcleaner
 brew cask install --appdir="/Applications" the-unarchiver
 brew cask install --appdir="/Applications" iterm2
-brew cask install --appdir="/Applications" sourcetree
-brew cask install --appdir="/Applications" sketch
-brew cask install --appdir="/Applications" flux
+brew cask install --appdir="/Applications" franz
+brew cask install --appdir="/Applications" dropbox
+brew cask install --appdir="/Applications" keeweb
 
 # Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook qlvideo

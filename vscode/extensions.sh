4bdfdd5d07e548dedec50d7bf0c276dd9219e01d
Adrien Denat <adrien.denat@gmail.com>
2018-11-09 16:18:04 +0100

    add vscode settings

---
+++ vscode/extensions.sh
@@ -0,0 +1,11 @@
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension EditorConfig.EditorConfig
+code --install-extension esbenp.prettier-vscode
+code --install-extension flowtype.flow-for-vscode
+code --install-extension Framer.framer-syntax
+code --install-extension k3a.theme-dark-plus-contrast
+code --install-extension msjsdiag.debugger-for-chrome
+code --install-extension Orta.vscode-jest
+code --install-extension silvenon.mdx
+code --install-extension whatwedo.twig
+code --install-extension wix.glean

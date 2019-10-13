da26547c5438503360bc2f022a24d225fcc366b8
Adrien Denat <adrien.denat@gmail.com>
2019-10-13 16:20:26 -0300

    update apps

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
+dockutil --no-restart --add "/Applications/Firefox.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
 dockutil --no-restart --add "/Applications/Sourcetree.app"
-dockutil --no-restart --add "/Applications/Sublime Text.app"
+dockutil --no-restart --add "/Applications/Visual Studio Code.app"
+dockutil --no-restart --add "/Applications/Franz.app"
+dockutil --no-restart --add "/Applications/KeeWeb.app"
 dockutil --no-restart --add "/Applications/Sketch.app"
 dockutil --no-restart --add "/Applications/Slack.app"
-dockutil --no-restart --add "/Applications/iTerm.app"
 dockutil --no-restart --add "/Applications/Calendar.app"
 
 killall Dock

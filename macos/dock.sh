95fcc546c1cc399d7cb28ee2f5b25789d34e7ebf
Adrien Denat <adrien.denat@gmail.com>
2020-01-02 10:46:27 -0300

    add dock autohide

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 
+# Set the icon size of Dock items to 35 pixels
+defaults write com.apple.dock tilesize -int 35
+
+# Set the dock to autohide
+defaults write com.apple.Dock autohide 1
+
+# Set the dock autohide delay to 0
+defaults write com.apple.Dock autohide-delay -float 0
+
 dockutil --no-restart --remove all
 dockutil --no-restart --add "/Applications/Firefox.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"

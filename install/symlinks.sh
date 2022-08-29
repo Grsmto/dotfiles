c4e95f1c01ab202b631707ad6fe09b273bb6c40a
Adrien Denat <adrien.denat@gmail.com>
2022-08-29 17:09:18 +0100

    remove SourceTree custom action undefined

--- install/symlinks.sh
+++ install/symlinks.sh
@@ -1,4 +1,3 @@
 ln -sfv "$DOTFILES_DIR/dots/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/dots/.gitignore_global" ~
 ln -sfv "$DOTFILES_DIR/dots/.zshrc" ~
-ln -sfv "$DOTFILES_DIR/editors/SourceTree/actions.plist" "$HOME/Library/Application Support/SourceTree/actions.plist"

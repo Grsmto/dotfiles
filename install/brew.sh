7d022f843405ba2b1747c4ed37dfb8dd6528078e
Adrien Denat <adrien.denat@gmail.com>
2022-08-29 16:13:57 +0100

    Update brew cast deprecated command

--- install/brew.sh
+++ install/brew.sh
@@ -13,7 +13,6 @@ brew install git
 brew install python
 brew install wget
 brew install yarn
-brew install unrar
 brew install wifi-password
 brew install mkcert
 
@@ -25,31 +24,31 @@ echo "Success! Basic brew packages are installed."
 
 # Install cask packages
 
-brew cask install --appdir="/Applications" google-chrome
-brew cask install --appdir="/Applications" firefox-developer-edition
-brew cask install --appdir="/Applications" brave-browser
-brew cask install --appdir="/Applications" slack
-brew cask install --appdir="/Applications" spotify
-brew cask install --appdir="/Applications" visual-studio-code
-brew cask install --appdir="/Applications" postman
-brew cask install --appdir="/Applications" insomnia
-brew cask install --appdir="/Applications" appcleaner
-brew cask install --appdir="/Applications" the-unarchiver
-brew cask install --appdir="/Applications" iterm2
-brew cask install --appdir="/Applications" ferdi
-brew cask install --appdir="/Applications" dropbox
-brew cask install --appdir="/Applications" keeweb
-brew cask install --appdir="/Applications" flux
-brew cask install --appdir="/Applications" imageoptim
-brew cask install --appdir="/Applications" toggl
-brew cask install --appdir="/Applications" sourcetree
-brew cask install --appdir="/Applications" diffmerge
-brew cask install --appdir="/Applications" figma
-brew cask install --appdir="/Applications" handbrake
-brew cask install --appdir="/Applications" raycast
-brew cask install --appdir="/Applications" rectangle
-brew cask install --appdir="/Applications" transmission
-brew cask install --appdir="/Applications" vlc
+brew install --appdir="/Applications" --cask google-chrome
+brew install --appdir="/Applications" --cask firefox-developer-edition
+brew install --appdir="/Applications" --cask brave-browser
+brew install --appdir="/Applications" --cask slack
+brew install --appdir="/Applications" --cask spotify
+brew install --appdir="/Applications" --cask visual-studio-code
+brew install --appdir="/Applications" --cask postman
+brew install --appdir="/Applications" --cask insomnia
+brew install --appdir="/Applications" --cask appcleaner
+brew install --appdir="/Applications" --cask the-unarchiver
+brew install --appdir="/Applications" --cask iterm2
+brew install --appdir="/Applications" --cask ferdi
+brew install --appdir="/Applications" --cask dropbox
+brew install --appdir="/Applications" --cask keeweb
+brew install --appdir="/Applications" --cask flux
+brew install --appdir="/Applications" --cask imageoptim
+brew install --appdir="/Applications" --cask toggl
+brew install --appdir="/Applications" --cask sourcetree
+brew install --appdir="/Applications" --cask diffmerge
+brew install --appdir="/Applications" --cask figma
+brew install --appdir="/Applications" --cask handbrake
+brew install --appdir="/Applications" --cask raycast
+brew install --appdir="/Applications" --cask rectangle
+brew install --appdir="/Applications" --cask transmission
+brew install --appdir="/Applications" --cask vlc
 
 # Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook qlvideo

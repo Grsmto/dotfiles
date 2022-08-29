7cf8373d98191ff461f3e7412dbd0b56beedea27
Adrien Denat <adrien.denat@gmail.com>
2022-08-29 15:50:05 +0100

    Update brew.sh

--- install/brew.sh
+++ install/brew.sh
@@ -15,6 +15,7 @@ brew install wget
 brew install yarn
 brew install unrar
 brew install wifi-password
+brew install mkcert
 
 # Wait a bit before moving on...
 sleep 1
@@ -25,12 +26,12 @@ echo "Success! Basic brew packages are installed."
 # Install cask packages
 
 brew cask install --appdir="/Applications" google-chrome
-brew cask install --appdir="/Applications" firefox
+brew cask install --appdir="/Applications" firefox-developer-edition
+brew cask install --appdir="/Applications" brave-browser
 brew cask install --appdir="/Applications" slack
 brew cask install --appdir="/Applications" spotify
 brew cask install --appdir="/Applications" visual-studio-code
 brew cask install --appdir="/Applications" postman
-brew cask install --appdir="/Applications" spectacle
 brew cask install --appdir="/Applications" appcleaner
 brew cask install --appdir="/Applications" the-unarchiver
 brew cask install --appdir="/Applications" iterm2
@@ -42,6 +43,12 @@ brew cask install --appdir="/Applications" imageoptim
 brew cask install --appdir="/Applications" toggl
 brew cask install --appdir="/Applications" sourcetree
 brew cask install --appdir="/Applications" diffmerge
+brew cask install --appdir="/Applications" figma
+brew cask install --appdir="/Applications" handbrake
+brew cask install --appdir="/Applications" raycast
+brew cask install --appdir="/Applications" rectangle
+brew cask install --appdir="/Applications" transmission
+brew cask install --appdir="/Applications" vlc
 
 # Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
 brew cask install qlcolorcode qlstephen qlmarkdown quicklook-json qlprettypatch quicklook-csv betterzipql qlimagesize webpquicklook qlvideo

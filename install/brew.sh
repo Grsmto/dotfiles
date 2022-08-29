fe275731ab868cfeab766aaaabdffef6cd57d603
Adrien Denat <adrien.denat@gmail.com>
2022-08-29 16:23:32 +0100

    Update Homebrew install

--- install/brew.sh
+++ install/brew.sh
@@ -1,6 +1,6 @@
 # Install Homebrew
 
-ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew update
 brew upgrade
 

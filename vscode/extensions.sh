6fc4f52b9f6fbf819b3e449748970cefcefe6645
Adrien Denat <oi@adriendenat.com>
2022-08-30 11:38:53 +0100

    remove deprecated vscode extension

--- vscode/extensions.sh
+++ vscode/extensions.sh
@@ -3,7 +3,5 @@ code --install-extension EditorConfig.EditorConfig
 code --install-extension esbenp.prettier-vscode
 code --install-extension Framer.framer-syntax
 code --install-extension k3a.theme-dark-plus-contrast
-code --install-extension msjsdiag.debugger-for-chrome
 code --install-extension Orta.vscode-jest
-code --install-extension silvenon.mdx
 code --install-extension wix.glean

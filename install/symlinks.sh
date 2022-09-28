9b3090ea40468a7c813bb1b3b4403efdb0e4cdae
Adrien Denat <oi@adriendenat.com>
2022-09-28 19:59:36 +0100

    add Node alias

--- install/symlinks.sh
+++ install/symlinks.sh
@@ -1,3 +1,5 @@
 ln -sfv "$DOTFILES_DIR/dots/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/dots/.gitignore_global" ~
 ln -sfv "$DOTFILES_DIR/dots/.zshrc" ~
+
+ln -s "$(which node)" /System/Volumes/Data/usr/local/bin

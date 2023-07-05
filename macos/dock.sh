998cc62dc1d343f141874a77953ba52b312b5d59
Adrien Denat <oi@adriendenat.com>
2023-07-05 11:32:33 +0100

    update dock items

--- macos/dock.sh
+++ macos/dock.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
 
 dockutil --no-restart --remove all
-dockutil --no-restart --add "/Applications/Brave.app"
+dockutil --no-restart --add "/Applications/Brave Browser.app"
 dockutil --no-restart --add "/Applications/Firefox.app"
 dockutil --no-restart --add "/Applications/Google Chrome.app"
 dockutil --no-restart --add "/Applications/Sourcetree.app"
 dockutil --no-restart --add "/Applications/Visual Studio Code.app"
-dockutil --no-restart --add "/Applications/Ferdi.app"
+dockutil --no-restart --add "/Applications/Ferdium.app"
 dockutil --no-restart --add "/Applications/KeeWeb.app"
 dockutil --no-restart --add "/Applications/Slack.app"
 dockutil --no-restart --add "/Applications/Spotify.app"
 dockutil --no-restart --add "/Applications/Calendar.app"
 
-dockutil --no-restart --add ~/Applications --display folder
+dockutil --add ~/Applications --display folder
 dockutil --add ~/Downloads
 
 killall Dock

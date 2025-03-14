5ca1e3484993b362f66f045c8a88dd13e3e3d220
Adrien Denat <oi@adriendenat.com>
2025-03-14 00:16:46 +0000

    add missing brew load

--- install/brew.sh
+++ install/brew.sh
@@ -1,6 +1,9 @@
 # Install Homebrew
 
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+eval "$(/opt/homebrew/bin/brew shellenv)"
+
 brew update
 brew upgrade
 
